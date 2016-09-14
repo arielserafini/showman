8b70f261881ac9320bce7ebbb46db678eb0fa63b
Ariel Serafini <ariel.serafini@wmg.com>
2016-09-14 14:27:03 -0300

    WIP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    shows: []
+  };
+
+  updateShows(shows) {
+    this.setState({
+      shows: shows
+    });
+  }
+
+  componentDidMount() {
+    axios.get('http://api.tvmaze.com/schedule').then(data => this.updateShows(data.data));
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>showman</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ShowList shows={this.state.shows}/>
       </div>
+    );
+  }
+}
+
+class ShowList extends Component {
+  render() {
+    let showItems = this.props.shows.map(show => {
+      return (
+        <li key={show.id}>
+          <Show data={show}/>
+        </li>
+      );
+    });
+    return (
+      <ul>
+        {showItems}
+      </ul>
+    );
+  }
+}
+
+class Show extends Component {
+  state = this.props.data;
+
+  render() {
+    return (
+      <span>{this.state.name}</span>
     );
   }
 }
